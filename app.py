78ef00a60f90fb13046f31db82c7e4df1f34dc75
Jashanpreet Singh <jashankhaira52@gmail.com>
2024-11-30 20:27:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello from Jashanpreet Singh GCP Cloud Run Container'
+    return 'Hello from Jashanpreet Singh Azure Container App'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
